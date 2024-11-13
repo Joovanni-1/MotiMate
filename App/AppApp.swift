c39bf6c52adbc9fef1e9bf4ee9c44117fdbf3ca2
Giovanni <giovannigaudiuso03@gmail.com>
2024-11-13 15:55:28 +0100

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Giovanni Gaudiuso on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
