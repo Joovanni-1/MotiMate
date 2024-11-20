16574c3f0e6330ec5aa9f695ee706843e262f6ac
Joovanni-1 <gio3vanni.gaudius0@gmail.com>
2024-11-20 18:18:51 +0100

    20/11

--- App/Nickname.swift
+++ App/Nickname.swift
@@ -11,8 +11,11 @@ struct Nickname: View{
     @State var nickname: String = ""
     @State var globalName: String = ""
     @State var cognome: String = ""
-    @State var age: Int = 15
-    @State var showPicker: Bool = false
+    @State var age: Int = 0
+    @State var sex: String = ""
+    @State var showPicker1: Bool = false
+    @State var showPicker2: Bool = false
+    let genders: [String]=["Maschio","Femmina","Altro  "]
     var body: some View {
         
         
@@ -29,8 +32,8 @@ struct Nickname: View{
                     .fontWeight(.heavy)
                     .multilineTextAlignment(.leading)
                     .padding(.leading,20)
-                Spacer()
-                    .frame(height:200)
+               Spacer()
+                  .frame(height:200)
                 
                 TextField("Nickname", text: $nickname)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -45,7 +48,8 @@ struct Nickname: View{
                         .font(.headline)
                     Spacer()
                     Button(action: {
-                        showPicker.toggle()
+                        showPicker1.toggle()
+                        if showPicker1 { showPicker2 = false }
                     }, label: {
                         Text("Seleziona la tua età")
                             .padding(8)
@@ -53,32 +57,88 @@ struct Nickname: View{
                             .foregroundColor(.white)
                             .cornerRadius(8)
                     })
-                       }
-                                }
-                                .padding(.horizontal)
-                                .padding(.bottom,450)
-                                
-                                
-                                if showPicker  {
-                                    Picker("Età", selection: $age) {
-                                        ForEach(0..<100) { number in
-                                            Text("\(number)").tag(number)
-                                        }
-                                    }
-                                    .pickerStyle(WheelPickerStyle())
-                                    .background(Color.white)
-                                    .padding(.top)
-                                    .frame(height: 90)
-                                    .cornerRadius(8)
-                                   // .onChange(of: age){_ in showPicker = false}
-                                    
-                                }
-            
-                                
-                                Spacer()
                 }
                 
+                
+                
+                
+                HStack { Text("Sesso: \(sex)")
+                        .font(.headline)
+                    Spacer()
+                    Button(action: {
+                        showPicker2.toggle()
+                        if showPicker2 { showPicker1 = false }
+                    }, label: {
+                        Text("Seleziona il tuo sesso")
+                            .padding(8)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    })
+                }
+
+                
+                
+                
+                
+                    
+                 
+                
+                
+            }  //fine VStack
+            .padding(.horizontal)
+            .offset(y:-200)
+         
+            
+            VStack{
+                Spacer()
+                if showPicker2  {
+                    Picker("Sesso", selection: $sex) {
+                        ForEach(genders, id:\.self) { gender in
+                            Text("\(gender)")
+                               .tag(gender)
+                        }
+                    }
+                  .pickerStyle(MenuPickerStyle())
+                    //.background(Color.white)
+                    .padding()
+                   
+                    .frame(height: 90)
+                    .cornerRadius(8)
+                    .onChange(of: sex){_ in showPicker2 = false }
+                    .offset(x:-20,y:-258.4)
+                   //.offset(y:-200)
+                }
+                    
+                
+                // func
+                if showPicker1  {
+                    Picker("Età", selection: $age) {
+                        ForEach(0..<100) { number in
+                            Text("\(number)").tag(number)
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    
+                    .background(Color.white)
+                   
+                    .cornerRadius(10)
+                    
+                    .padding(.all,60)
+                    
+                    .onChange(of: age){_ in showPicker1 = false}
+                    
+                } //fine
+                
+                
             }
+            
+            
+        }
+    }
+    
+    
+   
             
         }
         
