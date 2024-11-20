40600a227229c61a70f428b5433efd4691ebadac
Joovanni-1 <gio3vanni.gaudius0@gmail.com>
2024-11-20 09:49:08 +0100

    1

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -8,16 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isTrue :Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    ZStack{
+                        Image("1schermata")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: geometry.size.width, height: 1200)
+                            .clipped()
+                    ZStack{
+                           Text("Ciao, benvenuto su MotiMate!")
+                            .frame(width:300,height:150)
+                            
+                            .background(Color.verdino.opacity(0.85))
+                            
+                                .cornerRadius(20)
+                                .shadow(radius: 50)
+                            
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+                                
+                            
+                        }.padding(.bottom,400)
+                        Image(systemName:"chevron.down.2")
+                            .font(.system(size:60))
+                            .foregroundColor(.white)
+                            .padding(.top,350)
+                        
+                        Button(action: {
+                            isTrue.toggle()
+                        }, label: {Text("INIZIA")
+                            
+                        })
+                        .frame(width:100,height:50)
+                        
+                        .background(Color.blue)
+                            .cornerRadius(20)
+                            .shadow(radius: 50)
+                        
+                            .font(.system(size:26))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(.top,800)
+                            .fullScreenCover(isPresented:$isTrue,content:{
+                                Nickname()
+                            })
+                    }
+                }
+            }.frame(width: geometry.size.width)
+                .edgesIgnoringSafeArea(.all)
+                .scrollIndicators(.hidden)
         }
-        .padding()
     }
-}
+        
+    }
+    
+
 
 #Preview {
     ContentView()
--- App/Nickname.swift
+++ App/Nickname.swift
@@ -4,4 +4,98 @@
 //
 //  Created by Giovanni Gaudiuso on 14/11/24.
 //
+import SwiftUI
 
+
+struct Nickname: View{
+    @State var nickname: String = ""
+    @State var globalName: String = ""
+    @State var cognome: String = ""
+    @State var age: Int = 15
+    @State var showPicker: Bool = false
+    var body: some View {
+        
+        
+        ZStack{
+            Color.bluino
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Spacer()
+                    .frame(height:130)
+                Text("Iniziamo...")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.custom("Avenir", size: 36))
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.leading)
+                    .padding(.leading,20)
+                Spacer()
+                    .frame(height:200)
+                
+                TextField("Nickname", text: $nickname)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                TextField("Inserisci il tuo nome", text: $globalName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                TextField("Inserisci il tuo cognome", text: $cognome)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                HStack { Text("Età: \(age)")
+                        .font(.headline)
+                    Spacer()
+                    Button(action: {
+                        showPicker.toggle()
+                    }, label: {
+                        Text("Seleziona la tua età")
+                            .padding(8)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    })
+                       }
+                                }
+                                .padding(.horizontal)
+                                .padding(.bottom,450)
+                                
+                                
+                                if showPicker  {
+                                    Picker("Età", selection: $age) {
+                                        ForEach(0..<100) { number in
+                                            Text("\(number)").tag(number)
+                                        }
+                                    }
+                                    .pickerStyle(WheelPickerStyle())
+                                    .background(Color.white)
+                                    .padding(.top)
+                                    .frame(height: 90)
+                                    .cornerRadius(8)
+                                   // .onChange(of: age){_ in showPicker = false}
+                                    
+                                }
+            
+                                
+                                Spacer()
+                }
+                
+            }
+            
+        }
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+#Preview {
+    Nickname()
+}
